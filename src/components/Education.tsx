6c8857e399b7659f2cefec04f1f34dde492bc76d
samueldias99 <112509036+samueldias99@users.noreply.github.com>
2025-11-01 22:46:03 -0300

    feat: mover certificados para public e corrigir caminhos

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -28,7 +28,7 @@ const Education = () => {
       skills: ['TCP/IP', 'Protocolos de Rede', 'Firewalls', 'VPN', 'Análise de Tráfego'],
       icon: BookOpen,
       color: 'cyber-green',
-      imageUrl: '/src/img/redesseginfo.jpg',
+      imageUrl: '/certificados/redesseginfo.jpg',
       linkUrl: 'https://udemy-certificate.s3.amazonaws.com/image/UC-222a336b-ae67-4b56-bcd4-53ca08de3815.jpg?v=1754264487000'
     },
     {
@@ -41,7 +41,7 @@ const Education = () => {
       skills: ['Python', 'Ethical Hacking', 'Automação', 'Web Scraping', 'Network Programming'],
       icon: BookOpen,
       color: 'cyber-purple',
-      imageUrl: '/src/img/pythonparahackers.png',
+      imageUrl: '/certificados/pythonparahackers.png',
       linkUrl: 'https://www.udemy.com/course/python-para-hackers'
     }
   ];
@@ -53,7 +53,7 @@ const Education = () => {
       hours: 40,
       date: '2025',
       description: 'Programa intensivo de cibersegurança oferecido pelo Santander em parceria com a DIO, focado em hacking ético, testes de invasão e engenharia social para desenvolver habilidades estratégicas e fortalecer a segurança de sistemas.',
-      imageUrl: '/src/img/certificadocyber2025.png',
+      imageUrl: '/certificados/certificadocyber2025.png',
       linkUrl: 'https://assets.dio.me/Lj99MdC_nSwWHGhItCgOBeQyzs3g8Ukyk4yag5HwSPs/f:webp/h:320/q:70/w:450/L2NlcnRpZmljYXRlcy9jb3Zlci9VUUFFU0RRQS5qcGc'
     },
     {
@@ -62,7 +62,7 @@ const Education = () => {
       hours: 8,
       date: '2025',
       description: 'Formação focada no domínio do ecossistema Git e GitHub, capacitando profissionais em controle de versão, colaboração e administração de projetos. Habilidades desenvolvidas incluem gerenciamento de repositórios, workflows colaborativos, code reviews e práticas DevOps modernas.',
-      imageUrl: '/src/img/certificadogithub.png',
+      imageUrl: '/certificados/certificadogithub.png',
       linkUrl: 'https://assets.dio.me/AoogI8xWdR2Tx1uplWU6wDlxexbeGY0fsnAysgniPvU/f:webp/h:320/q:70/w:450/L2NlcnRpZmljYXRlcy9jb3Zlci80UkxSQzlIUC5qcGc'
     }
   ];
